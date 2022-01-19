c5b2201ddfec10644833e8410c8ac377cc6d77a7
117marta <117marta@gmail.com>
2022-01-19 23:33:27 +0100

    Model Institution

--- donation/donation_app/models.py
+++ donation/donation_app/models.py
@@ -6,3 +6,19 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Institution(models.Model):
+    TYPES = (
+        (1, 'Fundacja'),
+        (2, 'Organizacja pozarządowa'),
+        (3, 'Zbiórka lokalna'),
+    )
+
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    type = models.SmallIntegerField(choices=TYPES, default=1, max_length=1)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return f'{self.name}: {self.get_type_display()}'
