f1094bcf29ef793ddeb1f0be341bb4f5e5cdbc73
117marta <117marta@gmail.com>
2022-01-19 21:39:06 +0100

    Start app

---
+++ donation/local_settings.py
@@ -0,0 +1,9 @@
+DATABASES = {
+    'default': {
+        'HOST': '127.0.0.1',
+        'NAME': 'charity_donation',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
+    }
+}
