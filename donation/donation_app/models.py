1ddf330485a5e5b1442a8a6ad37bc2a6943238eb
117marta <117marta@gmail.com>
2022-01-19 23:45:40 +0100

    Model Donation

--- donation/donation_app/models.py
+++ donation/donation_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -22,3 +23,17 @@ class Institution(models.Model):
 
     def __str__(self):
         return f'{self.name}: {self.get_type_display()}'
+
+
+class Donation(models.Model):
+    quantity = models.SmallIntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    city = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=6)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
