d9f97f0179b5520702556f0639049addc1bf1841
117marta <117marta@gmail.com>
2022-01-20 13:45:10 +0100

    LandingPage - dynamic statistics

--- donation/donation_app/views.py
+++ donation/donation_app/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Category, Institution, Donation
 
 
 class LandingPage(View):
+    def bag_count_quantity(self):
+        total = 0
+        for bag in Donation.objects.all():
+            total += bag.quantity
+        return total
+
     def get(self, request):
-        return render(request, 'donation_app/index.html')
+        # bag_count = Donation.objects.all().count()
+        institution_count = len(Institution.objects.all().distinct())  # bez duplikatów
+        ctx = {
+            'bag_count': self.bag_count_quantity,
+            'institution_count': institution_count}
+        return render(request, 'donation_app/index.html', context=ctx)
 
 
 class AddDonation(View):
